a6bde56a36c2c10968234dc47c52e59cee6e1c4c
Pavel Kučera <pave.kucera@gmail.com>
2020-06-07 09:34:28 +0200

    Add missing blank line

--- test/Expect.ts
+++ test/Expect.ts
@@ -1,5 +1,7 @@
 import * as E from 'fp-ts/lib/Either'
 
-export const expectLeft = (expected: unknown, actual: unknown): void => expect(actual).toMatchObject(E.left(expected))
+export const expectLeft = (expected: unknown, actual: unknown): void =>
+  expect(actual).toMatchObject(E.left(expected))
 
-export const expectRight = (expected: unknown, actual: unknown): void => expect(actual).toMatchObject(E.right(expected))
+export const expectRight = (expected: unknown, actual: unknown): void =>
+  expect(actual).toMatchObject(E.right(expected))
